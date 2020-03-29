08e9e1fdba9824dfe86cfc4424f207d08631038c
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-29 20:56:55 +0000

    Automatically backed up by Learn

--- challange_kyle.rb
+++ challange_kyle.rb
@@ -7,6 +7,6 @@ def to_initials(name)
 end
 
 
-puts to_initials("Kelvin Bridges")      # => "KB"
-puts to_initials("Michaela Yamamoto")   # => "MY"
+p to_initials("Kelvin Bridges")      # => "KB"
+p to_initials("Michaela Yamamoto")   # => "MY"
 puts to_initials("Mary La Grange")      # => "MLG"
